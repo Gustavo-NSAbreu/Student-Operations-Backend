252d8673eb1f1a07358057bf2ab5d5c127177eca
Gustavo Abreu <guustavoabreu99@gmail.com>
2024-03-16 23:06:21 -0300

    Initial commit

---
+++ src/main/java/com/web_development_class/student_operations_backend/StudentOperationsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.web_development_class.student_operations_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentOperationsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentOperationsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web_development_class/student_operations_backend/StudentOperationsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web_development_class.student_operations_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentOperationsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
